808a86768528877dc8e85d0e0bdd32d982c526df
Aliyu <sadiqful@gmail.com>
2023-09-09 08:05:44 +0100

    feat: isEditable should be a boolean

--- src/models/category-model.ts
+++ src/models/category-model.ts
@@ -7,7 +7,7 @@ const categorySchema = new mongoose.Schema(
             required: true,
         },
         isEditable: {
-            type: String,
+            type: Boolean,
             required: false,
             default: true,
         },
