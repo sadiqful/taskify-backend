0b8ddc09c23e6550cf4837ce39712f8587f2a98a
Aliyu <sadiqful@gmail.com>
2023-10-15 18:22:21 +0100

    routes added to the server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import connectToDb from './db'
+import categoryRoutes from "./routes/category.routes"
+import taskRoutes from "./routes/task.routes"
+import userRoutes from "./routes/user.routes"
 
 const app = express();
 
@@ -14,6 +17,10 @@ const PORT = 1337
 app.get("ping", ( request: Request, response: Response) => {
     response.send("Hello World")
 })
+
+app.use("/users", userRoutes)
+app.use("/categories", categoryRoutes)
+app.use("/tasks", taskRoutes)
 
 app.listen(PORT, () => {
     console.log("Connection established")
