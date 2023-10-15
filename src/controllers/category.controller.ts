b08e880c06571453339cd3fb5b1d78d1fac8a6d1
Aliyu <sadiqful@gmail.com>
2023-10-15 18:06:42 +0100

    feat: get category by Id controller

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -17,3 +17,27 @@ export const getAllCategories = async (request: AuthRequest, response: Response)
       throw error
     }
   }
+
+  /**
+ *
+ * @param request TODO: Record get category by id
+ * @param response
+ * @returns
+ */
+
+export const getCategoryById = async (request: AuthRequest, response: Response) => {
+    try {
+      const { user } = request
+      const { id } = request.params
+      const category = await Category.findOne({
+        _id: id,
+      })
+      return response.send(category)
+    } catch (error) {
+      response.send({ error: "Something went wrong" })
+      console.log("error in getAllCategories", error)
+      throw error
+    }
+  }
+
+  
